fb56f75428422550c0dbf7799fe9fdb176ce4e3d
Taegon Lee <ltg9amer@naver.com>
2024-01-02 14:59:40 +0900

    20240102_4
    Delete Entity class and add IDamageable interface

--- Assets/01.Scripts/Player/Player.cs
+++ Assets/01.Scripts/Player/Player.cs
@@ -1,11 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class Player : Entity
+public class Player : MonoBehaviour, IDamageable
 {
-    public override void Init()
-    {
+    [SerializeField]
+    private UnityEvent _onDieTrigger;
+    [SerializeField]
+    private InputReader _inputReader;
+    [SerializeField]
+    private float _moveSpeed;
+    private CharacterController _characterController;
+    UnityEvent IDamageable.OnDieTrigger => _onDieTrigger;
 
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    public void Movement()
+    {
+        _characterController.Move(_inputReader.movementDirection * _moveSpeed * Time.deltaTime);
     }
 }
--- Assets/Settings/InputSettings/InputReader.cs
+++ Assets/Settings/InputSettings/InputReader.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,18 +7,22 @@ using UnityEngine.InputSystem;
 [CreateAssetMenu(menuName = "ScriptableObject/InputReader")]
 public class InputReader : ScriptableObject, Controls.IPlayerActions
 {
+    public event Action onReloadEvent;
+    public event Action onShootEvent;
+    public Vector2 movementDirection;
+
     public void OnMovement(InputAction.CallbackContext context)
     {
-
+        movementDirection = context.ReadValue<Vector2>();
     }
 
     public void OnReload(InputAction.CallbackContext context)
     {
-
+        onReloadEvent?.Invoke();
     }
 
     public void OnShoot(InputAction.CallbackContext context)
     {
-
+        onShootEvent?.Invoke();
     }
 }
