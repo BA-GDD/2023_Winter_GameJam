fdbe9ca5a7dffb0c73e01192572a4a261329f227
Gusdnd01 <cestop42@naver.com>
2024-01-02 14:58:50 +0900

    공격추가

--- Assets/01.Scripts/Enemy/MeleeAttack.cs
+++ Assets/01.Scripts/Enemy/MeleeAttack.cs
@@ -21,6 +21,8 @@ public class MeleeAttack : EnemyAttack
             {
                 //collider.GetComponent<Player>().TakeDamage();
             }
+
+            _attackTimer = 0;
         }
     }
 }
