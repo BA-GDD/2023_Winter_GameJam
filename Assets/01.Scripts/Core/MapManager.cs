971db21fd2dce0355ec91abeb0c312b3b6a7c169
hueam <72968569+hueam@users.noreply.github.com>
2024-01-02 14:12:42 +0900

    add checkWater

---
+++ Assets/01.Scripts/Core/MapManager.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public enum TileType
+{
+    Ground,
+    Water
+}
+public class MapManager : MonoBehaviour
+{
+    private static MapManager _instnace;
+    public static MapManager instance
+    {
+        get
+        {
+            if (_instnace == null)
+            {
+                _instnace = FindObjectOfType<MapManager>();
+            }
+            if (_instnace)
+            {
+                Debug.LogError($"존재하지않다[{typeof(MapManager)}].");
+            }
+            return _instnace;
+        }
+    }
+
+    [SerializeField] private Tilemap _groundMap;
+    [SerializeField] private Tilemap _holeMap;
+
+    [SerializeField] private TileBase _holeTile;
+    [SerializeField] private TileBase _groundTile;
+
+    [SerializeField] private Vector2Int defaultSpaSize;
+
+
+    private void Start()
+    {
+        Vector2Int minPos = Vector2Int.zero - defaultSpaSize / 2;
+        Vector2Int maxPos = Vector2Int.zero + defaultSpaSize / 2;
+        _holeMap.BoxFill(Vector3Int.zero, _holeTile, minPos.x, minPos.y, maxPos.x, maxPos.y);
+    }
+
+    public bool CheckWater(Vector3 pos)
+    {
+        pos.z = 0;
+        return _holeMap.HasTile(Vector3Int.CeilToInt(pos));
+    }
+
+
+    public void SetTile(Vector2 pos, TileType type)
+    {
+        Vector3Int intVec = new Vector3Int(Mathf.CeilToInt(pos.x), Mathf.CeilToInt(pos.y));
+        print(intVec);
+        DrawTile(intVec, type);
+    }
+    private void DrawTile(Vector3Int pos, TileType type)
+    {
+        Vector3Int minPos = pos - Vector3Int.one;
+        Vector3Int maxPos = pos + Vector3Int.one;
+        switch (type)
+        {
+            case TileType.Ground:
+                for (int x = minPos.x; x <= maxPos.x; x++)
+                {
+                    for (int y = minPos.y; y <= maxPos.y; y++)
+                    {
+                        _holeMap.SetTile(new Vector3Int(x, y), null);
+                    }
+                }
+                break;
+            case TileType.Water:
+                for (int x = minPos.x; x < maxPos.x; x++)
+                {
+                    for (int y = minPos.y; y < maxPos.y; y++)
+                    {
+                        _holeMap.SetTile(new Vector3Int(x, y), _holeTile);
+                    }
+                }
+                //_holeMap.SetTile(new Vector3Int(pos.x, pos.y), _holeTile);
+                //_groundMap.SetTile(new Vector3Int(pos.x, pos.y), null);
+                break;
+        }
+    }
+}
