bd135164760dad309a5af3d2f6396a5d488e54fd
Gusdnd01 <cestop42@naver.com>
2024-01-03 12:37:40 +0900

    CamShake Feedback 완성함ㅇㅇ

---
+++ Assets/01.Scripts/Feedbacks/FeedbackPlayer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FeedbackPlayer : MonoBehaviour
+{
+    [SerializeField] private List<Feedback> _feedbackList = new List<Feedback>();
+
+    private void Awake()
+    {
+        //GetComponents<Feedback>(_feedbackList);
+    }
+
+    public void PlayFeedback()
+    {
+        foreach(Feedback feedback in _feedbackList)
+        {
+            feedback.CompleteFeedback();
+            feedback.CreateFeedback();
+        }
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.gKey.wasPressedThisFrame)
+        {
+            PlayFeedback();
+        }
+    }
+}
