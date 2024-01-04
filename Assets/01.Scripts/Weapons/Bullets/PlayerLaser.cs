9d815a83ca8173489f3e170b3d28f17ca36d76d2
Taegon Lee <ltg9amer@naver.com>
2024-01-05 00:57:05 +0900

    20240105
    Modify laser gun effect

--- Assets/01.Scripts/Weapons/Bullets/PlayerLaser.cs
+++ Assets/01.Scripts/Weapons/Bullets/PlayerLaser.cs
@@ -5,43 +5,46 @@ using UnityEngine;
 public class PlayerLaser : MonoBehaviour
 {
     [HideInInspector]
-    public ParticleSystem particle;
+    public ParticleSystem rootParticle;
+    [HideInInspector]
+    public ParticleSystem shootParticle;
 
     [Header("Shoot Laser Properties")]
     [SerializeField]
     private float _shootStartLifeTime = 0.5f;
     [SerializeField]
-    private float _shootStartSizeX = 10f;
+    private float _shootStartSizeX = 1f;
     [SerializeField]
-    private float _shootStartSizeY = 0.1f;
+    private float _shootStartSizeY = 1f;
 
     [Header("Skill Laser Properties")]
     [SerializeField]
     private float _skillStartLifeTime = 3f;
     [SerializeField]
-    private float _skillStartSizeX = 25f;
+    private float _skillStartSizeX = 2.5f;
     [SerializeField]
-    private float _skillStartSizeY = 1f;
+    private float _skillStartSizeY = 10f;
 
-    private ParticleSystem.MainModule _mainParticle;
+    private ParticleSystem.MainModule _shootMainModule;
 
     private void Awake()
     {
-        particle = transform.Find("line").GetComponent<ParticleSystem>();
-        _mainParticle = particle.main;
+        rootParticle = GetComponent<ParticleSystem>();
+        shootParticle = transform.Find("shoot").GetComponent<ParticleSystem>();
+        _shootMainModule = shootParticle.main;
     }
 
     public void SetToShootLaser()
     {
-        _mainParticle.startLifetime = _shootStartLifeTime;
-        _mainParticle.startSizeX = _shootStartSizeX;
-        _mainParticle.startSizeY = _shootStartSizeY;
+        _shootMainModule.startLifetime = _shootStartLifeTime;
+        _shootMainModule.startSizeX = _shootStartSizeX;
+        _shootMainModule.startSizeY = _shootStartSizeY;
     }
 
     public void SetToSkillLaser()
     {
-        _mainParticle.startLifetime = _skillStartLifeTime;
-        _mainParticle.startSizeX = _skillStartSizeX;
-        _mainParticle.startSizeY = _skillStartSizeY;
+        _shootMainModule.startLifetime = _skillStartLifeTime;
+        _shootMainModule.startSizeX = _skillStartSizeX;
+        _shootMainModule.startSizeY = _skillStartSizeY;
     }
 }
